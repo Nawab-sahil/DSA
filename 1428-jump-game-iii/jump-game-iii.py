c55305e2b45a1d864b3267aa999d60e6a21141b2
Sahil <sahilnawab.web@gmail.com>
2026-05-17 23:55:41 +0530

    Time: 35 ms (35.58%) | Memory: 71.2 MB (5.77%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def canReach(self, arr, start):
+        """
+        :type arr: List[int]
+        :type start: int
+        :rtype: bool
+        """
+        
+        visited = set()
+        
+        def dfs(i):
+            if i < 0 or i >= len(arr) or i in visited:
+                return False
+            
+            if arr[i] == 0:
+                return True
+            
+            visited.add(i)
+            
+            return dfs(i + arr[i]) or dfs(i - arr[i])
+        
+        return dfs(start)
